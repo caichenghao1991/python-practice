c5fb00b0274fce9a84d30b2057679de48849d4ff
caichenghao1991 <caichenghao1991@gmail.com>
2021-07-30 16:04:25 -0500

    add gitignore

--- timer_01.py
+++ timer_01.py
@@ -15,7 +15,22 @@ def findNemo(list):
     duration = time() - start_time
     print('Method 1 call to find Nemo took ' + str(duration) + ' milliseconds')
 
+def findNemo2(list1, list2):
+    '''
+        find string nemo in 2 lists of strings
+        O(a+b) time complexity
+    '''
+    start_time = time()
+    for _ in list1:
+        if _ == 'Nemo':
+            print('Found Nemo')
+    for _ in list2:
+        if _ == 'Nemo':
+            print('Found Nemo')
+    duration = time() - start_time
+    print('Method 2 call to find Nemo took ' + str(duration) + ' milliseconds')
 
 if __name__ == '__main__': # Runs main() if file wasn't imported.
     # findNemo(names)
     findNemo(test_list)
+    findNemo2(test_list,test_list)
