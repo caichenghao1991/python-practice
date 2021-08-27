e2460c48f2d03957180202da3b34707fdfcc1364
caichenghao1991 <caichenghao1991@gmail.com>
2021-08-27 12:23:24 -0500

    queue

--- leetcode/array_01.py
+++ leetcode/array_01.py
@@ -42,7 +42,37 @@ class Solution(object):
         return area
 
 
+class MyQueue(object):
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int):
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if self.s2:
+            return self.s2[-1]
+        return self.s1[0]
+
+    def empty(self):
+        return not self.s1 and not self.s2
+
+
 if __name__ == "__main__":
     print(Solution.twoSum([2, 7, 11, 15], 9))
     print(Solution.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
     print(Solution.trap([4, 2, 0, 3, 2, 5]))
+    q = MyQueue()
+    q.push(1)
+    q.push(2)
+    print(q.pop())
+    print(q.peek())
+    print(q.empty())
